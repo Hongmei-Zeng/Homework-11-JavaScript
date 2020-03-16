ea82e3041ce942e75475b1d8671b947f0792600b
Hongmei Zeng <hongmei.lc@gmail.com>
2020-03-15 23:33:21 -0500

    Update files

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,29 @@
-const tableData = data
-const tbody = d3.select('tbody')
+const tbody = d3.select("tbody")
 
-function buildTable(data) {
-    tbody.html('')  // Clear existing data
+const button = d3.select("#filter-btn")
 
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
+const table = d3.select("#ufo-table").append("table")
+
+const thead = table.append("thead")
+
+const handler = function(){
+
+    tbody.html('')
+
+    const inputDate = d3.select("#datetime").property("value")
+
+    let filteredData = data.filter(e => e.datetime === inputDate)
+
+        filteredData.forEach(e => {
+        let row = tbody.append("tr")
+        row.append("td").text(e.datetime)
+        row.append("td").text(e.city)    
+        row.append("td").text(e.state)
+        row.append("td").text(e.country)
+        row.append("td").text(e.shape)
+        row.append("td").text(e.durationMinutes)
+        row.append("td").text(e.comments)
     })
 }
-
-const handleClick = () => {
-    d3.event.preventDefault()
-
-    const date = d3.select('#datetime').property('value')
-    let filteredData = tableData;
-
-    if (date)
-        filteredData = filteredData.filter(row => row.datetime === date)
-    
-    tbody.html('')  // Clear existing data
-
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
-    })
-}
-
-d3.selectAll('#filter-btn').on('click', handleClick)
-// buildTable(tableData)
+button.on("click", handler)
+input.on("change", handler)
